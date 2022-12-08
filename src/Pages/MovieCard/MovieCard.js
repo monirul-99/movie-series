622190db14727949b41cac20d842821f60e37b15
monaulic <sohelmuhammad164@gmail.com>
2022-12-08 13:25:41 +0600

    extra code remove

--- src/Pages/MovieCard/MovieCard.js
+++ src/Pages/MovieCard/MovieCard.js
@@ -1,25 +1,27 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Card from "./Card";
+import toast from "react-hot-toast";
 
 const MovieCard = () => {
   const [movieData, setMovieData] = useState([]);
-  const [error, setError] = useState("");
   useEffect(() => {
     axios
       .get(`https://api.tvmaze.com/search/shows?q=all`)
       .then((res) => setMovieData(res.data))
       .catch((error) => {
-        setError(error.message);
+        toast.error(error.message);
       });
   }, []);
   return (
     <div className="container mx-auto">
       <div className="uppercase font-Poppins mt-16">
         <div className="">
-          <h1 className="uppercase px-6 mb-3 text-white lg:text-3xl">
-            most <span>watches Movies</span>
-          </h1>
+          {movieData.length && (
+            <h1 className="uppercase px-6 mb-3 text-white lg:text-3xl">
+              most <span>watches Movies</span>
+            </h1>
+          )}
         </div>
       </div>
 
@@ -28,7 +30,6 @@ const MovieCard = () => {
           <Card key={inx} card={card}></Card>
         ))}
       </div>
-      <p className="text-red-400">{error}</p>
     </div>
   );
 };
